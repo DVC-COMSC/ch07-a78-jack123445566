4c87de7faa3629cfea9632fb8fa548ecbab3b478
jack123445566 <112512759+jack123445566@users.noreply.github.com>
2022-12-14 01:26:11 +0000

    2nd commit

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+words = ['are', 'arrow', 'amore', 'aspire', 'assertive', 'arrogant', 'bartender', 'carter']
 
-words = input('Enter the list: ').split()
+# Create an empty list to store the result
+result = []
 
-are = ['a', 'r', 'e']
-idxlst = []
-for i in range(len(words)):
-    for j in range(len(are)):
-        prev_idx = 0 if j == 0 else idxlst[j-1]
-        ret = words[i].find(are[j], prev_idx)
-        if ret == -1:
-            break
-        else:	
-            print (words[i], end=' ')
-print()
+# Iterate over each word in the list
+for word in words:
+  # Use the find() method to search for the substring 'are' in the word
+  index = word.find('are')
+  
+  # If the find() method returns a non-negative value, it means that the substring was found
+  # in the word, so we can add the word to the result list
+  if index >= 0:
+    result.append(word)
+
+# Print the result
+print(result)
