e10247b5aeba975fab4a88e5ad0973c7c76824d1
jack123445566 <112512759+jack123445566@users.noreply.github.com>
2022-12-14 01:36:22 +0000

    4th commit

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
-words = ['are', 'arrow', 'amore', 'aspire', 'assertive', 'arrogant', 'bartender', 'carter']
 
-# Create an empty list to store the result
-result = []
-
-# Define the list of letters to search for
 letters = ['a', 'r', 'e']
 
-# Iterate over each word in the list
+
+words = input("Enter a list of words: ").split()
+
+
+result = []
+
+
 for word in words:
-  # Create a list of the indices of the letters in the word
-  indices = [word.find(letter) for letter in letters]
   
-  # Check if the indices are in ascending order
-  if indices == sorted(indices):
-    # If the indices are in ascending order, it means that the word contains
-    # the letters in the correct sequence, so we can add it to the result list
+  idx = [word.find(letter) for letter in letters]
+  if idx == sorted(idx):
+   
     result.append(word)
 
-# Print the result
+
 print(result)
